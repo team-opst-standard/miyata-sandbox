99c4be24c98ff029e9eb816efa8712165df1299b
opst-miyatah <miyata.h@opst.co.jp>
2015-10-13 17:21:30 +0900

    [add]practice sources of vagrant and ansible

--- Vagrant/for_windows_host.sh
+++ Vagrant/for_windows_host.sh
@@ -4,12 +4,6 @@
 GROUP="windows_host"
 echo "Execution Start Ansible GROUP=$GROUP"
 
-echo "[command]PRIVATE_KEY_FILE=/vagrant/.vagrant/machines/default/virtualbox/private_key"
-PRIVATE_KEY_FILE="/vagrant/.vagrant/machines/default/virtualbox/private_key"
-
-echo "[command]export ANSIBLE_HOST_KEY_CHECKING=False"
-export ANSIBLE_HOST_KEY_CHECKING=False
-
 # install Ansible
 echo "[command]sudo yum install -y ansible"
 sudo yum install -y ansible
@@ -20,16 +14,10 @@ cat << EOS
 -i /vagrant/playbook/inventories/development 
 --connection=local 
 EOS
-# --private-key=$PRIVATE_KEY_FILE 
-# --extra-vars="provision_target=$GROUP" 
-# --verbose
 
 ansible-playbook /vagrant/playbook/site.yml \
 -i /vagrant/playbook/inventories/development \
 --connection=local \
 --verbose
-
-# --private-key=$PRIVATE_KEY_FILE \
-# --extra-vars="provision_target=$GROUP" \
 
 echo "End of execution Ansible GROUP=$GROUP"
---
+++ throw-in/create_roles_template.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+CURRENT=`pwd`
+ROLE_NAME=$1
+TARGET_DIR=${2:-$CURRENT}
+
+ROLES_DIRECTORIES=("handlers" "tasks" "template" "vars" "meta" "files" "defaults")
+GIT_KEEP_FILE=".gitkeep"
+MAIN_FILE="main.yml"
+
+yml_template() {
+cat << EOF >> $1
+- name: Hello, world!!
+  command: "/bin/echo 'Hello, world!!'"
+EOF
+}
+
+mkdir $TARGET_DIR/$ROLE_NAME
+for i in ${ROLES_DIRECTORIES[@]}; do
+    echo "mkdir $TARGET_DIR/$ROLE_NAME/$i"
+    mkdir $TARGET_DIR/$ROLE_NAME/$i
+
+    if [ "$i" = "tasks" ]; then
+        echo "touch $TARGET_DIR/$ROLE_NAME/$i/$MAIN_FILE"
+        touch $TARGET_DIR/$ROLE_NAME/$i/$MAIN_FILE
+        yml_template $TARGET_DIR/$ROLE_NAME/$i/$MAIN_FILE
+    else
+        echo "touch $TARGET_DIR/$ROLE_NAME/$i/$GIT_KEEP_FILE"
+        touch $TARGET_DIR/$ROLE_NAME/$i/$GIT_KEEP_FILE
+    fi
+done
+
+cd $CURRENT
+
